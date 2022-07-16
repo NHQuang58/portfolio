3191653f9b9c6b19a6e6347f1d68a8e71b0e7b96
NHQuang58 <qn052289@gmail.com>
2022-07-16 23:56:54 +0700

    add animation for navlink

--- src/components/Navbar/components/Navlink.jsx
+++ src/components/Navbar/components/Navlink.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-scroll";
 const Navlink = (props) => {
   const { to, display, isMobile, handleClick } = props;
   return (
-    <li className={`font-semibold ${isMobile && "py-6 text-4xl"}`}>
+    <li className={` duration-300 font-semibold hover:opacity-70 ${isMobile && "py-6 text-4xl"}`}>
       <Link onClick={handleClick} to={to} smooth={true} duration={500}>
         {display}
       </Link>
