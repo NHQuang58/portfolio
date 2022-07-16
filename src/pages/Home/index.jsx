0d796c0d58abe6b42354c672d0a198e891494cc5
NHQuang58 <qn052289@gmail.com>
2022-07-16 23:45:47 +0700

    home page: base UI

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Home = () => {
   return (
-    <div name="home" className="text-3xl">
-      Home
+    <div name="home" className="w-full h-screen bg-dark-background">
+      {/* Container */}
+      <div className="max-w-[1000px] h-full mx-auto px-8 flex flex-col justify-center">
+        <p className="text-xl text-pink-600">Hi, my name is</p>
+        <h1 className="text-4xl mb-2 sm:text-6xl font-bold text-[#ccd6f6] ">Nguyen Hong Quang</h1>
+        <h2 className="text-4xl text-[#8892b0]">I'm a software development engineer</h2>
+        <p className="text-[#8892b0] py-2 max-w-[700px]">
+          I have experience in developing embedded devices, and have almost 1 year of experience
+          programming firmware. But for now, I'm focusing on developing a complete software system
+          (backend and frontend).
+        </p>
+        <p className="text-[#8892b0] py-2 max-w-[700px]">
+          I am very eager to learn knowledge, both in terms of soft skills and technical skills. In
+          the near future, I want to become a software full stack developer.
+        </p>
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+            View Work
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3 " />
+            </span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
