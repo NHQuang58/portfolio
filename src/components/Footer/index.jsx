bf6083faac78e13bcc572c3554ddcf708ca756f5
NHQuang58 <qn052289@gmail.com>
2022-07-17 02:31:34 +0700

    change padding top for footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <div className="bg-dark-background pt-20 text-white w-full flex items-center justify-center">
+    <div className="bg-dark-background pt-8 text-white w-full flex items-center justify-center">
       <span className="mr-2">&copy; {currentYear}</span>Nguyen Hong Quang
     </div>
   );
